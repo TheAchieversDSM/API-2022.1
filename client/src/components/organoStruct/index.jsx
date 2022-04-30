ae3f505241a8fb007ceeb9fb3807bf60de48865a
Matheus.Sakuragui <matheus.lemesakuragui@gmail.com>
2022-04-30 14:18:43 -0300

    Adição de informações no organograma

--- client/src/components/organoStruct/index.jsx
+++ client/src/components/organoStruct/index.jsx
@@ -19,27 +19,43 @@ export default class extends Component {
 
     componentDidUpdate() {
         OrgChart.templates.polina.menuButton =
-            '<div style="position:absolute;right:{p}px;top: 10px; width:40px;height:30px;cursor:pointer;" data-ctrl-menu="">'
-            + '<hr style="background-color: #3AC6C4 ;opacity: 1; height: 3px; width: 40px; border: none;">'
-            + '<hr style="background-color: #3AC6C4 ;opacity: 1; height: 3px; width: 40px; border: none;">'
-            + '<hr style="background-color: #3AC6C4 ;opacity: 1; height: 3px; width: 40px; border: none;">'
+            '<div style="position:absolute;right:{p}px;top: 10px; width:40px;height:30px;cursor:pointer" data-ctrl-menu="rect">'
+            + '<hr style="background-color: #3AC6C4 ;opacity: 1; height: 3px; width: 40px; border: none;  fill="#039BE5">'
+            + '<hr style="background-color: #3AC6C4 ;opacity: 1; height: 3px; width: 40px; border: none;  fill="#039BE5">'
+            + '<hr style="background-color: #3AC6C4 ;opacity: 1; height: 3px; width: 40px; border: none; fill="#039BE5">'
             + '</div>';
         OrgChart.SEARCH_PLACEHOLDER = "Pesquisar";
         OrgChart.templates.polina.link = '<path stroke-linejoin="round" stroke="#aeaeae" stroke-width="1px" fill="none" d="{edge}" />';
         OrgChart.templates.polina.node = '<rect x="0" y="0" height="80" width="300" fill="#039BE5" stroke-width="4" stroke="#3AC6C4" rx="40" ry="40"></rect>';
         OrgChart.templates.polina.field_0 = '<text width="130" style="font-size: 18px;" fill="#000000" x="150" y="45" text-anchor="middle" class="field_0">{val}</text>';
         OrgChart.templates.polina.field_1 = '<text width="130" text-overflow="multiline" style="font-size: 12px;" fill="#ffffff" x="270" y="20" text-anchor="end" class="field_1">{val}</text>';
+        OrgChart.templates.polina.editFormHeaderColor = '#53C4CD';
 
         this.chart = new OrgChart(this.divRef.current, {
-            mode: "dark",
             template: "polina",
             layout: OrgChart.treeLeftOffset,
             enableSearch: false,
             mouseScrool: OrgChart.action.none,
+            
+            toolbar: {
+                zoom: true,
+                expandAll: true
+            },
             nodeBinding: {
-                field_0: "name",
-                field_1: "title",
-                img_0: "img"
+                field_0: "Nome",
+                field_1: "Cargo",
+                field_2: "Email",
+                field_3: "Departamento",
+                img_0: "foto"
+            },
+            editForm: {
+                titleBinding: "Nome",
+                buttons: {
+                    pdf: {text: "Exportar para PDF" },
+                    share: {text: "Compartilhar" },
+                    edit: null,
+                    remove: null
+                }
             },
             menu: {
                 pdf: { 
@@ -55,7 +71,7 @@ export default class extends Component {
                     icon: OrgChart.icon.svg(24, 24, "#53C4CD") 
                 },
                 csv: { 
-                    text: "Exportar para CSV",
+                    text: "Exportar para CSV" ,
                     icon: OrgChart.icon.csv(24, 24, "#53C4CD") 
                 }
             },
--- client/src/utils/organoNode/organoNode.js
+++ client/src/utils/organoNode/organoNode.js
@@ -1,7 +1,7 @@
 export function nodeCreate(lenArray, colaboradores) {
     var nodes = []
     for (let index = 0; index < lenArray; index++) {
-        const node = { id: colaboradores[index].con_id, name: colaboradores[index].con_nome, pid: colaboradores[index].head_id, title: colaboradores[index].car_descricao, img: "" }
+        const node = { id: colaboradores[index].con_id, Nome: colaboradores[index].con_nome, pid: colaboradores[index].head_id,Email:colaboradores[index].con_email, Departamento:colaboradores[index].dep_descricao  ,Cargo:colaboradores[index].car_descricao, foto: "" }
         nodes.push(node)
     }
     return (nodes)
